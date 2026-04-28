77e754acb1e4654a9c3cc74fd587ac5fbd985e4a
Shikhar <shikharvarshney10@gmail.com>
2026-04-28 09:32:16 +0530

    feat: next config update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "node:path";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.23"],
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  outputFileTracingRoot: path.resolve(__dirname),
 };
 
 export default nextConfig;
